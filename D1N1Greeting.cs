7e0d7751f69dc60e3989f47c998a5bc545c354f5
trashpanda95 <aubrian230@gmail.com>
2017-09-18 18:10:07 -0400

    first commit

---
+++ D1N1Greeting.cs
@@ -0,0 +1,21 @@
+using System;
+
+/*
+ * Program: D1N1Greeting
+ * Description: Displays a message
+ * Programmed by: Brian Au
+ */
+class D1N1Greeting
+{
+	static void Main ()
+	{
+		// Display a message
+		Console.WriteLine("My first C# program.");
+		Console.WriteLine("My name is "+"Brian");
+		Console.Write("My age is ");
+		Console.Write("22");
+
+		Console.WriteLine();
+	}
+
+}
